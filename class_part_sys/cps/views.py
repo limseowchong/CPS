c6f0dec4b0b87d157621f533afb8dd8c9c0fbb0a
limseowchong <lseowchong@gmail.com>
2022-08-11 17:46:53 +0800

    Created View Modules Page using Bootstrap table

--- class_part_sys/cps/views.py
+++ class_part_sys/cps/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Student, Module, Session, ClassParticipation
+
 # Create your views here.
 def index(request):
-    return render(request, "cps/index.html")
+    return render(request, "cps/index.html", {
+        "modules": list(Module.objects.all())
+    })
