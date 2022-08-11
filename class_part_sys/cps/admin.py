b5c7e7b3f078d12436ebc736d91e6b6b211f40f7
limseowchong <lseowchong@gmail.com>
2022-08-11 17:17:08 +0800

    RE: Created and Migrated the following models (Student, Module, Session, ClassParticipation)

--- class_part_sys/cps/admin.py
+++ class_part_sys/cps/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Student, Module, Session, ClassParticipation
+
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id", "student_id", "name")
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ("id", "module_code", "name", "section_id")
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(Session)
+admin.site.register(ClassParticipation)
--- class_part_sys/cps/models.py
+++ class_part_sys/cps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_id = models.IntegerField()
+    name = models.CharField(max_length=64)
+
+class Module(models.Model):
+    module_code = models.CharField(max_length=18)
+    name = models.CharField(max_length=64)
+    section_id = models.CharField(max_length=3)
+
+class Session(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    date = models.DateField()
+
+class ClassParticipation(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    score = models.IntegerField()
